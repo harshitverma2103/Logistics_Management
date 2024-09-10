c4bc3e274963b9362d459f9b009042b728a7b769
harshitverma2103 <harshitverma2103@gmail.com>
2024-09-10 20:24:11 +0530

    Remove button styling from index.css and add delete icon to card component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import './style.css'
+import React from "react";
+import deleteIcon from "../../assets/delete-icon.png";
+import "./style.css";
 
-const Card = ({ title, subTitle, description }) => {
+const Card = ({ title, subTitle, description, onDelete }) => {
   return (
     <div className="card">
       <div className="card-content">
         <h2>{title}</h2>
-        {subTitle && <h4>{subTitle}</h4>}
+        <h3>{subTitle}</h3>
         <p>{description}</p>
       </div>
+      {onDelete && (
+        <button className="card-delete-btn" onClick={onDelete}>
+          <img src={deleteIcon} alt="Delete" className="card-delete-icon" />
+        </button>
+      )}
     </div>
   );
 };
